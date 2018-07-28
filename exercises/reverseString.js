7a06bad0cb1978ec1b90c4b0577e974250fff3ef
Phanousit Syhakhom <psyhakhom@gmail.com>
2018-07-28 14:23:50 -0400

    completed reverse string

---
+++ exercises/reverseString.js
@@ -0,0 +1,13 @@
+function reverse(str) {
+	let reversed = ''
+
+	// for (let i = str.length - 1; i >= 0; i--) {
+	// 	reversed += str[i]
+	// }
+
+	for (let char of str) {
+		reversed = char + reversed
+	}
+
+	return reversed
+}
